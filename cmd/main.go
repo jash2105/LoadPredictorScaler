d3b94ec814e342980f6ad995c782d5dc250f37d0
Jashwanth Kumar K <101602768+jash2105@users.noreply.github.com>
2024-06-09 18:04:26 -0500

    api resources - triggerscaler

--- cmd/main.go
+++ cmd/main.go
@@ -33,6 +33,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	scalingv1alpha1 "github.com/jash2105/ScaleAheadOperator/api/v1alpha1"
+	"github.com/jash2105/ScaleAheadOperator/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -44,6 +47,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(scalingv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -119,6 +123,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.TriggerScalingReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "TriggerScaling")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
